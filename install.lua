5bda50d7e4994291d6c5414ef8b1e3678cce8a50
Elizabeth Fransen <elizabethlfransen@gmail.com>
2022-02-28 13:44:29 -0600

    prefer startup.lua over startup

--- install.lua
+++ install.lua
@@ -78,7 +78,7 @@ end
 
 local function createStartupFile()
     -- TODO append to the startup file in case they have something
-    local file = fs.open("startup", "w")
+    local file = fs.open("startup.lua", "w")
     local startupPath = installDir .. "/startup.lua"
     local execLine = "os.run({},\"" .. startupPath .. "\", \"" .. installDir .. "\")"
     file.write(execLine)
