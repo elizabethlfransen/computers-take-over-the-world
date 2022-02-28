ce8f5fa0068938a1912dbf50571f2f233027876a
Elizabeth Fransen <elizabethlfransen@gmail.com>
2022-02-28 12:46:42 -0600

    Add install directions to readme

--- install.lua
+++ install.lua
@@ -1,6 +1,7 @@
 local baseUrl = "https://api.github.com/repos/elizabethlfransen/computers-take-over-the-world"
 local branch = "main"
 local installDir = "computers-take-over-the-world"
+local branchFile = "branch.json"
 local createStartup = true
 
 local function getRemoteHash()
@@ -14,16 +15,38 @@ local function getRemoteContents()
     return textutils.unserialiseJSON(response.readAll())
 end
 
-local function install()
-    print("Installing first time")
+local function getLocalContents()
+    local path = installDir .. "/" .. branchFile
+    if ~fs.exists(path) then
+        return nil
+    end
+    local file = fs.open(path, "r")
+    local result = file.readAll()
+    file.close()
+    return result
 end
 
-local function update()
-    print("Updating")
+local function updateLocalContents(contents)
+    local serializedContents = textutils.serializeJSON(contents)
+    local path = installDir .. "/" .. branchFile
+    local file = fs.open(path, "w")
+    file.write(serializedContents)
+    file.close()
+end
+
+local function install()
+    print("Getting branch contents...")
+    local localContents = getLocalContents()
+    local remoteContents = getRemoteContents()
+    updateLocalContents(remoteContents)
 end
 
 if fs.exists(installDir) then
+    print("Updating")
     install()
 else
-    update()
+    print("Installing first time")
+    print("Creating directory " .. installDir)
+    fs.makeDir(installDir)
+    install()
 end
--- readme.md
+++ readme.md
@@ -1,2 +1,8 @@
 # Computers take over the world
 This is a computercraaft program that runs on a turtle. The turtle will then consume resources in the world and reproduce.
+
+## Installing
+```
+wget https://raw.githubusercontent.com/elizabethlfransen/computers-take-over-the-world/main/install.lua
+install
+```
